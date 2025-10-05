a27244a92e5239a9a48800341b9a44397adaeb5b
VenkataRao <venkataraolinux4@gmail.com>
2025-10-05 12:55:29 +0530

    varibles

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0f5a4423ccb46e300" #replace with you security group id 
+ZONE_ID="Z0358867T36L0L2AAO3E"
+DOMAIN_NMAE="venkatr.fun"
 
 for instance in $@
 do
@@ -10,9 +12,32 @@ do
 
     if [ $instance != "frontend" ]; then 
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NMAE" #mongodb.venkatr.fun
+
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
     fi
 
     echo "$instance: $IP"
+    
+    # Creates route 53 records based on env name
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Testing creating a record set"
+        ,"Changes": [{
+         "Action"              : "CREATE"
+        ,"ResourceRecordSet"  : {
+         "Name"              : "'$RECORD_NAME'"
+         ,"Type"             : "A"
+         ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+            "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '
 done
